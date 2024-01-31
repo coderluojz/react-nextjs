05f968f63baef1f9711d1d3bb7659c01732178b5
luojz <929747115@qq.com>
2024-01-31 11:13:11 +0800

    feat: 增加移入偏移动画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,95 +49,103 @@ const AppPage = () => {
           </span>
           <div className="scroll-item item1">
             <div className="hover-item">{HoverItem1Svg}</div>
-            <div className="source-item">{Item1_Svg}</div>
-            <div className="content-item">
-              <div>
-                <span className="line line1"></span>
-                <h3 className="title">Introduction to programming</h3>
-                <div className="tag-box">
-                  <span className="tag">Beginner</span>
+            <div className="source-item">
+              {Item1_Svg}
+              <div className="content-item">
+                <div>
+                  <span className="line line1"></span>
+                  <h3 className="title">Introduction to programming</h3>
+                  <div className="tag-box">
+                    <span className="tag">Beginner</span>
+                  </div>
+                  <p className="text">
+                    This course covers the most basic concepts in programming
+                    using Solidity as an example.
+                  </p>
                 </div>
-                <p className="text">
-                  This course covers the most basic concepts in programming
-                  using Solidity as an example.
-                </p>
-              </div>
-              <div className="footer">
-                <div className="item">
-                  {TimeSvg} <span>36 Hour</span>
-                </div>
-                <div className="item">
-                  {BookSvg} <span>5 Course</span>
-                </div>
-                <div className="item item-btn">
-                  {AircraftSvg} <span>45% COMPLETED</span>
+                <div className="footer">
+                  <div className="item">
+                    {TimeSvg} <span>36 Hour</span>
+                  </div>
+                  <div className="item">
+                    {BookSvg} <span>5 Course</span>
+                  </div>
+                  <div className="item item-btn">
+                    {AircraftSvg} <span>45% COMPLETED</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="scroll-item item2">
             <div className="hover-item">{HoverItem2Svg}</div>
-            <div className="source-item">{Item2_Svg}</div>
-            <div className="content-item">
-              <div>
-                <h3 className="title">Moonshot 2023 Summer Hackathon</h3>
-                <div className="tag-box">
-                  <span className="tag">All Tracks</span>
-                  <span className="tag">Solidity</span>
-                  <span className="tag">ZK</span>
+            <div className="source-item">
+              {Item2_Svg}
+              <div className="content-item">
+                <div>
+                  <h3 className="title">Moonshot 2023 Summer Hackathon</h3>
+                  <div className="tag-box">
+                    <span className="tag">All Tracks</span>
+                    <span className="tag">Solidity</span>
+                    <span className="tag">ZK</span>
+                  </div>
                 </div>
-              </div>
-              <div className="footer-list">
-                {ListData.map((item) => {
-                  return (
-                    <div className="list-item" key={item.label}>
-                      <span className="label">{item.label}</span>
-                      <span className="value">{item.value}</span>
-                    </div>
-                  )
-                })}
+                <div className="footer-list">
+                  {ListData.map((item) => {
+                    return (
+                      <div className="list-item" key={item.label}>
+                        <span className="label">{item.label}</span>
+                        <span className="value">{item.value}</span>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
             </div>
           </div>
           <div className="scroll-item item3">
             <div className="hover-item">{HoverItem3Svg}</div>
-            <div className="source-item">{Item3_Svg}</div>
-            <div className="content-item">
-              <div>
-                <span className="line line2"></span>
-                <h3 className="title">Web 3.0 Programming Basics</h3>
-                <div className="tag-box">
-                  <span className="tag">Beginner</span>
+            <div className="source-item">
+              {Item3_Svg}
+              <div className="content-item">
+                <div>
+                  <span className="line line2"></span>
+                  <h3 className="title">Web 3.0 Programming Basics</h3>
+                  <div className="tag-box">
+                    <span className="tag">Beginner</span>
+                  </div>
+                  <p className="text">
+                    Basic concepts in programming of Solidity. Topics include:
+                    variables, functions, flow control, error handling, data
+                    structure.
+                  </p>
                 </div>
-                <p className="text">
-                  Basic concepts in programming of Solidity. Topics include:
-                  variables, functions, flow control, error handling, data
-                  structure.
-                </p>
-              </div>
-              <div className="footer">
-                <div className="item">
-                  {TimeSvg} <span>36 Hour</span>
-                </div>
-                <div className="item">
-                  {BookSvg} <span>5 Course</span>
+                <div className="footer">
+                  <div className="item">
+                    {TimeSvg} <span>36 Hour</span>
+                  </div>
+                  <div className="item">
+                    {BookSvg} <span>5 Course</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="scroll-item item4">
             <div className="hover-item">{HoverItem4Svg}</div>
-            <div className="source-item">{Item4_Svg}</div>
-            <div className="content-item">
-              <span className="line line3"></span>
-              <div className="footer footer-image-text">
-                <div className="item">
-                  {TimeSvg} <span>36 Hour</span>
+            <div className="source-item">
+              {Item4_Svg}
+              <div className="content-item">
+                <span className="line line3"></span>
+                <div className="footer footer-image-text">
+                  <div className="item">
+                    {TimeSvg} <span>36 Hour</span>
+                  </div>
+                  <span className="text">What is Bitcoin</span>
                 </div>
-                <span className="text">What is Bitcoin</span>
               </div>
+              <div className="item-image">{Item5_Svg}</div>
             </div>
-            <div className="item-image">{Item5_Svg}</div>
           </div>
         </div>
       </div>
